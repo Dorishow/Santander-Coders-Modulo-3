d5e19c814ee6803c39bfa861f73fd50d2d644d09
samuelnfavero <92187894+samuelnfavero@users.noreply.github.com>
2021-10-15 21:08:25 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,16 @@
+class Book {
+
+    constructor(title, genre, author, date){
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.read = false;
+        this.date = date
+    }
+
+}
+
+class BookList{
+
+    construct
+}
